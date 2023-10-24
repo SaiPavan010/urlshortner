a13454e196303a87dce03193a167deace1619ae0
saipavan0010 <pavan.kavuturu@fission.it>
2023-10-24 17:03:45 +0530

    added mongodb

--- main.go
+++ main.go
@@ -1,38 +1,140 @@
+// package main
+
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// 	"net/http"
+// 	"sync"
+
+// 	"github.com/gorilla/mux"
+// )
+
+// // Shortener is a simple URL shortener
+// type Shortener struct {
+// 	mu      sync.Mutex
+// 	counter int
+// 	urls    map[string]string
+// }
+
+// // NewShortener creates a new URL shortener
+// func NewShortener() *Shortener {
+// 	return &Shortener{
+// 		urls: make(map[string]string),
+// 	}
+// }
+
+// // Shorten generates a short URL for the given long URL
+// func (s *Shortener) Shorten(longURL string) string {
+// 	s.mu.Lock()
+// 	defer s.mu.Unlock()
+
+// 	s.counter++
+// 	shortURL := base64.URLEncoding.EncodeToString([]byte(fmt.Sprint(s.counter)))
+
+// 	s.urls[shortURL] = longURL
+// 	return shortURL
+// }
+
+// // RedirectHandler redirects short URLs to their corresponding long URLs
+// func (s *Shortener) RedirectHandler(w http.ResponseWriter, r *http.Request) {
+// 	shortURL := mux.Vars(r)["shortURL"]
+// 	longURL, ok := s.urls[shortURL]
+// 	if !ok {
+// 		http.NotFound(w, r)
+// 		return
+// 	}
+
+// 	http.Redirect(w, r, longURL, http.StatusFound)
+// }
+
+// func (s *Shortener) urlShortner(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodPost {
+// 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+// 		return
+// 	}
+
+// 	longURL := r.FormValue("url")
+// 	if longURL == "" {
+// 		http.Error(w, "Missing 'url' parameter", http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	shortURL := s.Shorten(longURL)
+// 	fmt.Fprintf(w, "Shortened URL: http://localhost:8080/%s", shortURL)
+// }
+
+// func main() {
+// 	shortener := NewShortener()
+
+// 	router := mux.NewRouter()
+
+// 	// Handle root endpoint
+// 	router.HandleFunc("/{shortURL}", shortener.RedirectHandler).Methods("GET")
+
+// 	// Handle /shorten endpoint
+// 	router.HandleFunc("/shorten", shortener.urlShortner).Methods("POST")
+
+// 	fmt.Println("Server listening on :8080")
+// 	http.ListenAndServe(":8080", router)
+// }
+
 package main
 
 import (
+	"context"
 	"encoding/base64"
 	"fmt"
 	"net/http"
 	"sync"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Shortener is a simple URL shortener
 type Shortener struct {
-	mu      sync.Mutex
-	counter int
-	urls    map[string]string
+	mu         sync.Mutex
+	counter    int
+	urls       map[string]string
+	collection *mongo.Collection
 }
 
-// NewShortener creates a new URL shortener
+// NewShortener creates a new URL shortener and initializes MongoDB connection
 func NewShortener() *Shortener {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	database := client.Database("urlshortener")
+	collection := database.Collection("urls")
+
 	return &Shortener{
-		urls: make(map[string]string),
+		urls:       make(map[string]string),
+		collection: collection,
 	}
 }
 
-// Shorten generates a short URL for the given long URL
-func (s *Shortener) Shorten(longURL string) string {
+// Shorten generates a short URL for the given long URL and stores it in MongoDB
+func (s *Shortener) Shorten(longURL string) (string, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
 	s.counter++
 	shortURL := base64.URLEncoding.EncodeToString([]byte(fmt.Sprint(s.counter)))
 
+	_, err := s.collection.InsertOne(context.Background(), map[string]interface{}{
+		"shortURL": shortURL,
+		"longURL":  longURL,
+	})
+	if err != nil {
+		return "", err
+	}
+
 	s.urls[shortURL] = longURL
-	return shortURL
+	return shortURL, nil
 }
 
 // RedirectHandler redirects short URLs to their corresponding long URLs
@@ -40,8 +142,17 @@ func (s *Shortener) RedirectHandler(w http.ResponseWriter, r *http.Request) {
 	shortURL := mux.Vars(r)["shortURL"]
 	longURL, ok := s.urls[shortURL]
 	if !ok {
-		http.NotFound(w, r)
-		return
+		// Check MongoDB for the short URL
+		var result struct {
+			LongURL string `bson:"longURL"`
+		}
+		filter := map[string]interface{}{"shortURL": shortURL}
+		err := s.collection.FindOne(context.Background(), filter).Decode(&result)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		longURL = result.LongURL
 	}
 
 	http.Redirect(w, r, longURL, http.StatusFound)
@@ -59,7 +170,12 @@ func (s *Shortener) urlShortner(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	shortURL := s.Shorten(longURL)
+	shortURL, err := s.Shorten(longURL)
+	if err != nil {
+		http.Error(w, "Error creating short URL", http.StatusInternalServerError)
+		return
+	}
+
 	fmt.Fprintf(w, "Shortened URL: http://localhost:8080/%s", shortURL)
 }
 
